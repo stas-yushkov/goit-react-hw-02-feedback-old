cbcc7f685ca7ec5a1ab160bbc452d99592ca34d1
Stanislav Yushkov <s.yushkov@cargoo.ua>
2021-09-16 15:22:29 +0300

    second step Done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import { Component } from 'react';
+import feedbackOptions from './data/feedbackOptionsData.json'
+
+import React, { Component } from 'react';
+import FeedBackOptions from './components/FeedBackOptions/'
+import Statistics
+  from './components/Statistics';
+import { Container } from './components';
+import { Section } from './components/Section';
+
+const initialState = feedbackOptions.reduce((acc, option) => {
+    return {...acc, ...{ [option]: 1 }};
+  },{});
 
 class App extends Component {
+  state = initialState;
+
+  manageState = (e) => {
+    const name = e.target.dataset.name
+
+    this.setState((currentState) => ({ [name]: currentState[name] + 1 }))
+  }
+
   render() {
-    return <>{/* // // //*/}</>;
+    return (
+      <Section>
+        <Container>
+          <FeedBackOptions buttonList={feedbackOptions} handleIncrement={this.manageState} />
+          <Statistics stats={this.state}/>
+        </Container>
+      </Section>
+    )
   }
 }
 
